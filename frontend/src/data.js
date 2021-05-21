0455d9e38b33b444b7b7e8f6f4b4fb612788606c
darknight230796 <63100146+darknight230796@users.noreply.github.com>
2021-05-21 19:13:40 +0530

    save on git

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'MechWear Automaton',
+            category:'Shoes',
+            image:'/images/p1.jpg',
+            price:1200,
+            rating: 4.5,
+        },
+        {
+            _id:'2',
+            name:'MechWear Combustion',
+            category:'Shoes',
+            image:'/images/p2.jpg',
+            price:1300,
+            rating: 4.5,
+        },
+        {
+            _id:'3',
+            name:'MechWear Gear',
+            category:'Shoes',
+            image:'/images/p3.jpg',
+            price:1150,
+            rating: 4.5,
+        },
+        {
+            _id:'4',
+            name:'MechWear Velocity',
+            category:'Shoes',
+            image:'/images/p4.jpg',
+            price:1520,
+            rating: 5,
+        },
+        {
+            _id:'5',
+            name:'MechWear Vector',
+            category:'Shoes',
+            image:'/images/p5.jpg',
+            price:1250,
+            rating: 4.5,
+        },
+        {
+            _id:'6',
+            name:'MechWear Torque',
+            category:'Shoes',
+            image:'/images/p6.jpg',
+            price:1600,
+            rating: 5,
+        },
+    ]
+}
+
+export default data;
