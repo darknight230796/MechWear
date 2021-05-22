1eb1c1cbaabf943696f6eea2520636ca5754f1ad
darknight230796 <63100146+darknight230796@users.noreply.github.com>
2021-05-22 19:45:08 +0530

    cart add

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,23 @@
-import React from 'react';
-
+import React, { useEffect } from "react";
+import { addToCart } from "../actions/cartActions";
+import {useDispatch} from 'react-redux';
 export default function CartScreen(props) {
-    const productId = props.match.params.id;
-    const qty = props.location.search? Number(props.location.search.split('=')[1]) : 1;
-    return(
-        <div>
-            <h1>Cart Screen</h1>
-            <p>
-                ADD TO CART: ProductID : {productId} Qty : {qty}
-            </p>
-        </div>
-    )
+    const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search.split("=")[1])
+    : 1;
+  useEffect(() => {
+      if(productId){
+          dispatch(addToCart(productId,qty));
+      }
+  }, [dispatch,productId,qty]);
+  return (
+    <div>
+      <h1>Cart Screen</h1>
+      <p>
+        ADD TO CART: ProductID : {productId} Qty : {qty}
+      </p>
+    </div>
+  );
 }
