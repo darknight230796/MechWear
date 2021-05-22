9bb0110611e4a37ba088a091f92678c7b22eb285
darknight230796 <63100146+darknight230796@users.noreply.github.com>
2021-05-22 13:24:56 +0530

    eslint

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
-
+//const express = require('express');
+import data from './data.js' 
 const app = express();
-
+app.get('/api/products', (req,res)=>{
+    res.send(data.products);
+});
 app.get('/',(req,res)=>{
     res.send('Server is ready');
 });
