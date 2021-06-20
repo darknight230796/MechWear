5bf87797f284cbe7a762e6b986342cbd0b120dc6
darknight230796 <63100146+darknight230796@users.noreply.github.com>
2021-06-20 19:05:04 +0530

    test2

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ export default function HomeScreen() {
   const dispatch = useDispatch();
   const productList = useSelector(state => state.productList);
   const {loading, error, products} = productList;
-
+//
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
